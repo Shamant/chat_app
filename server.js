92e3ee17d2735d20dcb6b3a602b65eb0884e6fcf
Shamant <74949605+Shamant@users.noreply.github.com>
2026-07-22 00:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,28 +36,10 @@ const DIGEST_WINDOW_MS =
 let messages = [];
 let whiteboardPaths = [];
 const ALLOWED_EMAILS = new Set([
-  "ddevaraj@cisb.org.in",
-  "aajay@cisb.org.in",
-  "dchandrakanth@cisb.org.in",
-  "mechaudhari@cisb.org.in",
-  "skumar@cisb.org.in",
-  "ynavneeth@cisb.org.in",
-  "risharma@cisb.org.in",
-  "hvinoj@cisb.org.in",
-  "msundermurthy@cisb.org.in",
-  "snandagopal@cisb.org.in",
-  "amithra@cisb.org.in",
-  "asabnani@cisb.org.in",
-  "khomma@cisb.org.in",
-  "smanikanti@cisb.org.in",
-  "nchilakapati@cisb.org.in",
-  "pnair@cisb.org.in",
-  "shamantsai@gmail.com",
+  
 ]);
 const MODERATOR_EMAILS = new Set([
-  "smanikanti@cisb.org.in",
-  "pnair@cisb.org.in",
-  "shamantsai@gmail.com",
+  
 ]);
 const brevoSmtpLogin = globalThis.process?.env.BREVO_SMTP_LOGIN;
 const brevoSmtpPassword = globalThis.process?.env.BREVO_SMTP_PASSWORD;
